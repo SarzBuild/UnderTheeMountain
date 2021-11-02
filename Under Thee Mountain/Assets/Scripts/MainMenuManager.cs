f0a155253de68362d0dfbe7d3e3ad685d1b6a038
SarzBuild <jrmietn@gmail.com>
2021-11-01 20:03:14 -0400

    Some changes to some scripts

--- Under Thee Mountain/Assets/Scripts/MainMenuManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MainMenuManager : MonoBehaviour
-{
-    public void StartGame() => SceneManager.LoadScene("SampleScene");
-    public void ExitGame() => Application.Quit();
-}
--- Under Thee Mountain/Assets/Scripts/SceneManagement.cs
+++ Under Thee Mountain/Assets/Scripts/SceneManagement.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameStartBehaviour : MonoBehaviour
+public class SceneManagement : MonoBehaviour
 {
     public void StartGame() => SceneManager.LoadScene("SampleScene");
     public void ExitGame() => SceneManager.LoadScene("__main__");
+    public void ExitApp() => Application.Quit();
 }
